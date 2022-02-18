cbcd80a57ace52e2517f46c21cce7367d4f15dd6
wreising <wreising@mac.com>
2022-02-17 20:27:00 -0800

    m

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-document.getElementById('generate').addEventListener('click', () => {
+// document.getElementById('generate').addEventListener('click', () => {
 
-})
+// })
 
 let passlength = prompt("Please enter your password length.");
 
@@ -15,13 +15,13 @@ let lowerCase = confirm("Would you like lowercase?");
 let specialCharacters = confirm("Would you like special characters?");
 let numbers = confirm("Would you like numbers?");
 
-charsSUL = (upperCase && lowerCase && specialCharacters && (numbers == false));
-charsSU = (specialCharacters && upperCase);
-charsUL = (upperCase && lowerCase);
-charsSL = (lowerCase && specialCharacters)
-charsSULN = (upperCase && lowerCase && specialCharacters && numbers);
-charsSUN = (specialCharacters && upperCase && numbers);
-charsULN = (upperCase && lowerCase && numbers);
+// charsSUL = (upperCase && lowerCase && specialCharacters && (numbers == false));
+// charsSU = (specialCharacters && upperCase);
+// charsUL = (upperCase && lowerCase);
+// charsSL = (lowerCase && specialCharacters)
+// charsSULN = (upperCase && lowerCase && specialCharacters && numbers);
+// charsSUN = (specialCharacters && upperCase && numbers);
+// charsULN = (upperCase && lowerCase && numbers);
 
 console.log(passlength);
 console.log(upperCase);
@@ -29,16 +29,9 @@ console.log(lowerCase);
 console.log(specialCharacters);
 console.log(numbers);
 
-// var charsSUL = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-// var charsSU = "0123456789!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-// var charsl = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()"
-// var charUL = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-// var charU = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-// var charL = "0123456789abcdefghijklmnopqrstuvwxyz"
+// structure and math from https://stackoverflow.com/a/16548229/17557927
+// modified by me for different combinations of characters
 
-// function is from https://stackoverflow.com/a/16548229/17557927 - modified by me
-
-// if (charsSUL == true) {
 function password_generator(len) {
   var length = (len) ? (len) : (passlength);
   var stringLower = "abcdefghijklmnopqrstuvwxyz";
@@ -49,101 +42,36 @@ function password_generator(len) {
   var character = "";
   var crunch = true;
   while (password.length < length) {
-    // if (lowerCase == true) {
     entity1 = Math.ceil(stringLower.length * Math.random() * Math.random());
-    // } else if (lowerCase == false) {
-    // entity1 = ""
-    // }
     entity11 = Math.ceil(stringUpper.length * Math.random() * Math.random());
     entity2 = Math.ceil(numeric.length * Math.random() * Math.random());
     entity3 = Math.ceil(punctuation.length * Math.random() * Math.random());
     hold = stringLower.charAt(entity1);
     hold11 = stringUpper.charAt(entity11);
-    // if (uppercase = true) {
-    //   hold = (password.length % 2 == 0) ? (hold.toUpperCase()) : (hold);
-    // }
     if (lowerCase == true) {
       character += hold;
     } else if (lowerCase == false) {
-      character += ""
+      character += "";
     }
-    character += hold11;
-    character += numeric.charAt(entity2);
-    character += punctuation.charAt(entity3);
+    if (upperCase == true) {
+      character += hold11;
+    } else if (upperCase == false) {
+      character += "";
+    }
+    if (numbers == true) {
+      character += numeric.charAt(entity2);
+    } else if (numbers == false) {
+      character += "";
+    }
+    if (specialCharacters == true) {
+      character += punctuation.charAt(entity3);
+    } else if (specialCharacters == false) {
+      character += "";
+    }
     password = character;
   }
   password = password.split('').sort(function () { return 0.5 - Math.random() }).join('');
   return password.substr(0, len);
 }
-// } else if (charsUL == true) {
-//   function password_generator(len) {
-//     var length = (len) ? (len) : (10);
-//     var string = "abcdefghijklmnopqrstuvwxyz"; //to upper 
-//     var numeric = '0123456789';
-//     // var punctuation = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
-//     var password = "";
-//     var character = "";
-//     var crunch = true;
-//     while (password.length < passwordLength) {
-//       entity1 = Math.ceil(string.length * Math.random() * Math.random());
-//       // entity2 = Math.ceil(numeric.length * Math.random() * Math.random());
-//       // entity3 = Math.ceil(punctuation.length * Math.random() * Math.random());
-//       hold = string.charAt(entity1);
-//       hold = (password.length % 2 == 0) ? (hold.toUpperCase()) : (hold);
-//       character += hold;
-//       // character += numeric.charAt(entity2);
-//       // character += punctuation.charAt(entity3);
-//       password = character;
-//     }
-//     password = password.split('').sort(function () { return 0.5 - Math.random() }).join('');
-//     return password.substr(0, len);
-//   }
-// } else if (charsSL == true) {
-//   function password_generator(len) {
-//     var length = (len) ? (len) : (10);
-//     var string = "abcdefghijklmnopqrstuvwxyz"; //to upper 
-//     var numeric = '0123456789';
-//     var punctuation = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
-//     var password = "";
-//     var character = "";
-//     var crunch = true;
-//     while (password.length < passwordLength) {
-//       entity1 = Math.ceil(string.length * Math.random() * Math.random());
-//       // entity2 = Math.ceil(numeric.length * Math.random() * Math.random());
-//       entity3 = Math.ceil(punctuation.length * Math.random() * Math.random());
-//       hold = string.charAt(entity1);
-//       // hold = (password.length % 2 == 0) ? (hold.toUpperCase()) : (hold);
-//       character += hold;
-//       // character += numeric.charAt(entity2);
-//       character += punctuation.charAt(entity3);
-//       password = character;
-//     }
-//     password = password.split('').sort(function () { return 0.5 - Math.random() }).join('');
-//     return password.substr(0, len);
-//   }
-// } else if (charsULSN == true) {
-//   function password_generator(len) {
-//     var length = (len) ? (len) : (10);
-//     var string = "abcdefghijklmnopqrstuvwxyz"; //to upper 
-//     var numeric = '0123456789';
-//     var punctuation = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
-//     var password = "";
-//     var character = "";
-//     var crunch = true;
-//     while (password.length < passwordLength) {
-//       entity1 = Math.ceil(string.length * Math.random() * Math.random());
-//       entity2 = Math.ceil(numeric.length * Math.random() * Math.random());
-//       entity3 = Math.ceil(punctuation.length * Math.random() * Math.random());
-//       hold = string.charAt(entity1);
-//       hold = (password.length % 2 == 0) ? (hold.toUpperCase()) : (hold);
-//       character += hold;
-//       character += numeric.charAt(entity2);
-//       character += punctuation.charAt(entity3);
-//       password = character;
-//     }
-//     password = password.split('').sort(function () { return 0.5 - Math.random() }).join('');
-//     return password.substr(0, len);
-//   }
-// }
 
 console.log(password_generator());
