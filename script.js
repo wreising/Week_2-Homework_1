c5729229d1c4ea9ee1a03dc23a2bc8ee1ba1e2f1
wreising <wreising@mac.com>
2022-02-18 06:56:17 -0800

    m

--- script.js
+++ script.js
@@ -2,29 +2,37 @@
 
 // })
 
-let passlength = prompt("Please enter your password length.");
-
-if (passlength < 8) {
-  passlength = prompt("Passwords must be at least 8 characters. Enter the number of characters you would like.");
-} else if (passlength > 128) {
-  passlength = prompt("Passwords may not be greater that 128 characters. Enter the number of characters you would like.");
+function password_again() {
+  document.getElementById("password").innerHTML = "ok";
 }
 
-let upperCase = confirm("Would you like uppercase?");
-let lowerCase = confirm("Would you like lowercase?");
-let specialCharacters = confirm("Would you like special characters?");
-let numbers = confirm("Would you like numbers?");
+var btn = document.getElementById(Generate Password);
 
-console.log(passlength);
-console.log(upperCase);
-console.log(lowerCase);
-console.log(specialCharacters);
-console.log(numbers);
-
-// structure and math from https://stackoverflow.com/a/16548229/17557927
-// modified by me for different combinations of characters
+btn.addEventListener(“click”, password_generator());
 
 function password_generator(len) {
+
+  let passlength = prompt("Please enter your password length.");
+  if (passlength < 8) {
+    passlength = prompt("Passwords must be at least 8 characters. Enter the number of characters you would like.");
+  } else if (passlength > 128) {
+    passlength = prompt("Passwords may not be greater that 128 characters. Enter the number of characters you would like.");
+  }
+  let upperCase = confirm("Would you like uppercase?");
+  let lowerCase = confirm("Would you like lowercase?");
+  let specialCharacters = confirm("Would you like special characters?");
+  let numbers = confirm("Would you like numbers?");
+
+  console.log(passlength);
+  console.log(upperCase);
+  console.log(lowerCase);
+  console.log(specialCharacters);
+  console.log(numbers);
+
+  // structure and math from https://stackoverflow.com/a/16548229/17557927
+  // modified by me for different combinations of characters
+
+
   var length = (len) ? (len) : (passlength);
   var stringLower = "abcdefghijklmnopqrstuvwxyz";
   var stringUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -65,5 +73,3 @@ function password_generator(len) {
   password = password.split('').sort(function () { return 0.5 - Math.random() }).join('');
   return password.substr(0, len);
 }
-
-console.log(password_generator());
